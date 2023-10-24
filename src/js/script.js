a187f3177b999e68ae0783a8badfaec6ec3a3234
Breno Teixeira <brenotm10@gmail.com>
2023-10-24 17:42:25 -0300

    finalizando

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,7 @@
-// tooltip
-const tooltips = document.querySelectorAll('[data-tooltip]');
-
-tooltips.forEach((item)=> {
-  item.addEventListener('mouseover', onMouseOver);
-});
-
-function onMouseOver(event){
-  const tooltipBox = crialTooltipBox(this);
-  tooltipBox.style.top = event.pageY + 'px';
-  tooltipBox.style.left = event.pageX + 'px';
-
-  onMouseMove.tooltipBox = tooltipBox;
-  onMouseLeave.tooltipBox = tooltipBox;
-  onMouseLeave.element = this;
-  this.addEventListener('mouseleave', onMouseLeave)
-  this.addEventListener('mousemove', onMouseMove)
-}
-
-const onMouseLeave = {
-  handleEvent(){
-    this.tooltipBox.remove()
-    this.element.removeEventListener('mouseleave', onMouseLeave);
-    this.element.removeEventListener('mousemove', onMouseMove);
-  }
-}
-
-const onMouseMove = {
-  handleEvent(event){
-    this.tooltipBox.style.top = event.pageY + 20 + 'px';
-    this.tooltipBox.style.left = event.pageX + 20 + 'px';
-
-  }
-}
-
-
-function crialTooltipBox (element){
-  const tooltipBox = document.createElement('div');
-  const text = element.getAttribute('aria-label');
-  tooltipBox.classList.add('tooltip')
-  tooltipBox.innerText = text;
-  document.body.appendChild(tooltipBox);
-  return tooltipBox;
-};
-
-
-
-
-
-
 // funcionamento
 
-const funcionamento = document.querySelector('[data-semana]');
-  
+  const funcionamento = document.querySelector('[data-semana]');
+
   const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
   const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
 
@@ -63,7 +13,7 @@ const funcionamento = document.querySelector('[data-semana]');
   const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
   const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
 
-  if(semanaAberto && horarioAberto){
+  if (semanaAberto && horarioAberto) {
     funcionamento.classList.add('aberto');
   };
 
